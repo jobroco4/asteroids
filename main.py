0300a4cbaa94c12e32ece56cb329cf3f48927922
jobroco4 <jobroco4@gmail.com>
2025-09-11 17:42:12 -0600

    add fps counter

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -11,6 +13,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        pygame.time.Clock().tick(60)   # Limit to 60 FPS
+        dt = pygame.time.Clock().tick(60) / 1000  # Delta time in seconds
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")

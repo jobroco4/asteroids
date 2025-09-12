6f749984a0cd30c4b7e47bf7caf1b612479727fc
jobroco4 <jobroco4@gmail.com>
2025-09-11 21:46:05 -0600

    added player triangle shape and draw method

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         pygame.time.Clock().tick(60)   # Limit to 60 FPS
         dt = pygame.time.Clock().tick(60) / 1000  # Delta time in seconds
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,24 @@
+
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS
+import pygame
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        color = (255, 255, 255)  # white
+        pygame.draw.polygon(screen, color, self.triangle(), 2)
+
+    
+

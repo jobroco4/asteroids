7dd6eee67888266288e14ad8519b73ff23bb47dc
jobroco4 <jobroco4@gmail.com>
2025-11-15 14:43:08 -0700

    updates

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from asteroid import *
 from asteroidfield import *
 
 def main():
+    VERSION = pygame.version.ver
+    print(f"Starting Asteroids with pygame version: {VERSION}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.time.Clock()
